5e0be613993e13c88694fac7579fec1772a4a850
Sousa89 <kevin.de.sousa89@gmail.com>
2022-03-18 14:47:34 -0400

    fixed index issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+// import { Routes, Route } from "react-router-dom";
+
+// import MovieApi from "./Movie-api";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <Routes>
+      <Route></Route>
+    </Routes> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MovieApi.js
@@ -0,0 +1,37 @@
+// Movie-api.js
+
+// import axios
+import axios from "axios";
+// import state/ useEffect
+import { useState, useEffect } from "react";
+
+const MovieApi = () => {
+  // declare useState
+
+
+
+
+useEffect(() =>{
+
+  axios(()=>{
+    url ="https://api.themoviedb.org/3/search/movie",
+    
+    
+
+
+
+  }) 
+
+
+
+})
+  //wrap the api call in the useEffect State
+  // make the api call with axios
+
+
+
+
+  return <div></div>;
+};
+
+export default MovieApi;
