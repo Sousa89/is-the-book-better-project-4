0ce5982bd97913f3357d03ef870d37729d583457
Alvin Chou <alvinchou516@hotmail.com>
2022-03-29 16:48:23 -0400

    commit to merger

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -10,6 +10,5 @@ const BackgroundVideo = () => {
           <video autoPlay loop muted id="video1" className="video">
             <source src={backgroundVideo1} type="video/mp4" />
           </video>
-      }
-};
+  )};
 export default BackgroundVideo;
