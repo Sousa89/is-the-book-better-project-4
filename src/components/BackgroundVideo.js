09272b965f90146b012d36ed1afbb9de8d3e4343
Nightclaw6 <sclaudiu82@yahoo.com>
2022-03-29 18:48:39 -0400

    finished media queries

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,13 +1,8 @@
 import { useState } from "react";
 import backgroundVideo1 from "../videoFiles/library.mp4";
 import backgroundVideo2 from "../videoFiles/library2.mp4";
-<<<<<<< HEAD
-import backgroundVideo3 from "../videoFiles/film1.mp4";
-import backgroundVideo4 from "../videoFiles/film2.mp4";
-=======
 // import backgroundVideo3 from "../videoFiles/film1.mp4";
 // import backgroundVideo4 from "../videoFiles/film2.mp4";
->>>>>>> f55a9944f48c414b0135da7bfc23e33e08d896d5
 // import backgroundVideo5 from "../videoFiles/film3.mp4";
 
 const BackgroundVideo = () => {
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,17 @@
 // Title.js
-
+import useWindowDimensions from "./useWindowDimensions";
 const Title = (props) => {
+  const {width} = useWindowDimensions();
+  // const title = props.title;
+  console.log(width);
+  console.log(props.title.length);
+
   return (
     <div className="title">
-      <h1>{props.title} </h1>
+      {/* <h1>{props.title} </h1> */}
+      {
+        props.title.length < 40 || width > 520 ?  <h1>{props.title} </h1> : <p className="bigTitle">{props.title}</p> 
+      }
       {/* {
         props.book & props.movie
         ? null
